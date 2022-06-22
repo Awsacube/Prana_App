5401464aee3423959baf0eab114bad0fb8581e6b
Raghu <raghu@Raghus-MacBook-Air.local>
2022-06-22 11:34:36 +0530

    Adding changes to mainactivity as per react navigation new setup

--- android/app/src/main/java/com/pillbox/MainActivity.java
+++ android/app/src/main/java/com/pillbox/MainActivity.java
@@ -1,5 +1,5 @@
 package com.pillbox;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,4 +12,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "PillBox";
   }
+
+  @Override
+protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
+
 }

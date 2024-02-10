40d8f86ed8ea7fcf255e0c57b1bb62afd013a221
Hiteshk369 <parchahiteshkumar123@gmail.com>
2024-02-10 15:42:07 +0530

    int

--- src/screens/Profile/EditProfile.js
+++ src/screens/Profile/EditProfile.js
@@ -1,36 +1,20 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import Custombutton from '../../components/Custombutton';
+
 import {getToken} from '../../services/AsyncStorageService';
 import {
   useEditProfileMutation,
   useGetLoggedUserQuery,
 } from '../../services/userAuthApi';
+import Custominput from '../../components/Custominput';
+import {colors} from '../../constants/colors';
 
 const EditProfile = () => {
   const navigation = useNavigation();
 
-  const [first_name, setFirstName] = useState('');
-  const [last_name, setLastName] = useState('');
-  const [phone_number, setMobile] = useState('');
-  const [street_1, setStreet_1] = useState('');
-  const [street_2, setStreet_2] = useState('');
-  const [district, setDistrict] = useState('');
-  const [state, setState] = useState();
-  const [pincode, setPincode] = useState('');
   const [userLToken, setUserLToken] = useState();
-
-  // setFirstName(item.first_name)
-  // setLastName(item.last_name)
-  // setMobile(item.phone_number)
-  // setStreet_1(item.address.street_1)
-  // setStreet_2(item.address.street_2)
-  // setCity(item.address.city)
-  // setDistrict(item.address.district)
-  // setPincode(item.address.pincode)
-  // setState(item.address.state)
 
   useEffect(() => {
     const getT = async () => {
@@ -42,26 +26,28 @@ const EditProfile = () => {
 
   const [editProfile] = useEditProfileMutation(userLToken);
 
-  const Save = async () => {
-    const newData = {
-      first_name,
-      last_name,
-      phone_number,
-      userLToken,
-      city,
-      state,
-      district,
-      pincode,
-      street_1,
-      street_2,
-    };
-    const {data, isSuccess, error} = await editProfile(newData);
-    navigation.navigate('Home');
-  };
-
-  const {data, isSuccess} = useGetLoggedUserQuery(userLToken, {
+  const {data, isSuccess, refetch} = useGetLoggedUserQuery(userLToken, {
     refetchOnMountOrArgChange: true,
   });
+
+  const [userData, setUserData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+  });
+  useEffect(() => {
+    if (data) {
+      setUserData({
+        firstName: data?.first_name,
+        lastName: data?.last_name,
+        email: data?.email,
+        phoneNumber: data?.phone_number,
+      });
+    }
+  }, [data]);
+
+  const [edit, setEdit] = useState(false);
 
   const profile = [];
 
@@ -69,40 +55,64 @@ const EditProfile = () => {
     isSuccess && profile.push(data);
   }
 
+  const handleSave = async () => {
+    const newData = {
+      first_name: userData.firstName,
+      last_name: userData.lastName,
+      phone_number: userData.phoneNumber,
+      userLToken,
+    };
+    const {data, isSuccess, error} = await editProfile(newData);
+    refetch();
+    navigation.navigate('Account');
+  };
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View>
-        <FlatList
-          data={profile}
-          keyExtractor={(item, index) => item.uuid}
-          showsHorizontalScrollIndicator={false}
-          renderItem={({item, index}) => {
-            return (
-              <View style={styles.container}>
-                <Text>First Name</Text>
-                <TextInput
-                  defaultValue={item.first_name}
-                  type="outlined"
-                  style={styles.input}
-                  onChangeText={e => setFirstName(e)}
-                />
-                <Text>Second Name</Text>
-                <TextInput
-                  defaultValue={item.last_name}
-                  style={styles.input}
-                  onChangeText={e => setLastName(e)}
-                />
-                <Text>10 Digit Mobile</Text>
-                <TextInput
-                  defaultValue={item.phone_number}
-                  style={styles.input}
-                  onChangeText={e => setMobile(e)}
-                />
-                <Custombutton text="Save" onPress={Save} />
-              </View>
-            );
-          }}
-        />
+        <View style={styles.mb}>
+          <Custominput
+            label={'First Name'}
+            name={'firstName'}
+            value={edit ? userData.firstName : data?.first_name}
+            onChangeText={text => setUserData({...userData, firstName: text})}
+            disabled={!edit}
+          />
+        </View>
+        <View style={styles.mb}>
+          <Custominput
+            label={'Last Name'}
+            name={'lastName'}
+            value={edit ? userData.lastName : data?.last_name}
+            onChangeText={text => setUserData({...userData, lastName: text})}
+            disabled={!edit}
+          />
+        </View>
+        <View style={styles.mb}>
+          <Custominput
+            label={'First Name'}
+            name={'firstName'}
+            value={edit ? userData.email : data?.email}
+            onChangeText={text => setUserData({...userData, email: text})}
+            disabled={true}
+          />
+        </View>
+        <View style={styles.mb}>
+          <Custominput
+            label={'Phone Number'}
+            name={'phoneNumber'}
+            value={edit ? userData.phoneNumber : data?.phone_number}
+            onChangeText={text => setUserData({...userData, phoneNumber: text})}
+            disabled={!edit}
+          />
+        </View>
+        <Pressable onPress={() => setEdit(!edit)} style={styles.cancelButton}>
+          <Text style={styles.cancelButtonText}>
+            {edit ? 'Cancel' : 'Edit'}
+          </Text>
+        </Pressable>
+        <Pressable onPress={() => handleSave()} style={styles.button}>
+          <Text style={styles.buttonText}>Save</Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
@@ -115,11 +125,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 15,
+    paddingHorizontal: 15,
     borderWidth: 1,
-    margin: 5,
     borderRadius: 15,
   },
   container: {
     marginTop: 25,
+    paddingHorizontal: 15,
+  },
+  mb: {
+    marginBottom: 8,
+  },
+  button: {
+    marginTop: 14,
+    width: '100%',
+    backgroundColor: 'purple',
+    paddingVertical: 12,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: colors.pearlWhite,
+    fontWeight: '500',
+  },
+  cancelButton: {
+    marginTop: 14,
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'purple',
+    paddingVertical: 12,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  cancelButtonText: {
+    fontSize: 18,
+    color: 'purple',
+    fontWeight: '500',
   },
 });
